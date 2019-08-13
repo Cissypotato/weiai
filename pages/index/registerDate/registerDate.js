88466ae0dd9bac56b27ded6b6d0323987e935c01
angyueyue <1121027880@qq.com>
2019-08-13 15:16:14 +0800

    add

--- pages/index/registerDate/registerDate.js
+++ pages/index/registerDate/registerDate.js
@@ -1,6 +1,8 @@
 const app = getApp()
 Page({
    data: {
+      isWeekId:0,
+      isIndex:0,
       weekInfo:{},
       intervalInfo:{},
       isZg:false
@@ -45,9 +47,45 @@ Page({
          isZg: false
       })
    },
-   formSubmit(){
-      wx.navigateTo({
-         url: '/pages/index/registerInfo/registerInfo',
+   formSubmit(e){
+      let then = this
+      let week_id = then.data.isWeekId
+      let tel = e.detail.value.tel
+      let uname = e.detail.value.uname
+      let myreg = /^(0|86|17951)?(13[0-9]|15[012356789]|16[6]|19[89]]|17[01345678]|18[0-9]|14[579])[0-9]{8}$/
+      if (week_id != 0){
+         if(myreg.test(tel)){
+           wx.navigateTo({
+              url: '/pages/index/registerInfo/registerInfo',
+           })
+         }else{
+            wx.showToast({
+               title: '请输入正确的手机号码',
+               icon: 'none',
+               duration: 1000,
+            })
+         }
+      }else{
+         wx.showToast({
+            title: '请选择时段',
+            icon: 'none',
+            duration: 1000,
+         })
+      }
+   },
+   choice_week(event){
+      let then = this
+      let index = event.currentTarget.dataset.index
+      then.setData({
+         isIndex: index,
+         intervalInfo: then.data.weekInfo[index].day
+      })
+   },
+   choice_sd(event){
+      let then = this
+      let id = event.currentTarget.dataset.id
+      then.setData({
+         isWeekId: id,
       })
    }
 })
