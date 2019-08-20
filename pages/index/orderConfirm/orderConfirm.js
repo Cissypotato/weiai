957128980e6a50515196380c743a87df3855de2b
cissy <1395422644@qq.com>
2019-08-20 09:14:48 +0800

    接口-5

--- pages/index/orderConfirm/orderConfirm.js
+++ pages/index/orderConfirm/orderConfirm.js
@@ -1,102 +1,172 @@
 const app = getApp();
 Page({
-   data: {
-      isSite: true
-   },
-   onLoad: function(options) {
-      this.setData({
-         Id: options.id,
-         Num: options.num
-      })
-      this.getInfo()
-   },
-   onShow: function() {
+  data: {
+    isSite: true,
+    Id: "",
+    Num: '',
+    uid:''
+  },
+  onLoad: function(options) {
+    console.log(options)
+    this.setData({
+      Id: options.id,
+      Num: options.num
 
-   },
-   getInfo() {
-      let then = this
-      wx.request({
-         url: app.globalData.appUrl + 'drug/drug_num',
-         data: {
-            id: this.data.Id,
-            num: this.data.Num,
-            uid: 1
-         },
-         success: function(res) {
-            then.setData({
-               data: res.data
-            })
-         },
-         fail: function(res) {
-            wx.showToast({
-               title: '网络错误请稍后再试',
-               icon: 'none',
-               duration: 1000,
-            })
-         },
-      })
-   },
-   toAddress() {
-      wx.navigateTo({
-         url: '/pages/personal/address/address',
-      })
-   },
-   tjOrder() {
-      wx.request({
-         url: app.globalData.appUrl + '',
-         data: {
-            id: this.data.Id
-         },
-         header: {},
-         method: 'GET',
-         dataType: 'json',
-         responseType: 'text',
-         success: function(res) {
-            console.log(res)
-            let pay = res.data
-            let param = {
-               "timeStamp": pay.timeStamp,
-               "package": pay.packages,
-               "paySign": pay.paySign,
-               "signType": pay.signType,
-               "nonceStr": pay.nonceStr
-            };
-            this.payment(param)
-         },
-         fail: function(res) {
-            wx.showToast({
-               title: '请求失败请稍后再试',
-               icon: 'none',
-               duration: 1000,
-            })
-         },
-      })
-   },
-   payemnt(param) {
-      wx.requestPayment({
-         timeStamp: param.timeStamp,
-         nonceStr: param.nonceStr,
-         package: param.package,
-         signType: param.signType,
-         paySign: param.paySign,
-         success: function(res) {
+    })
+    this.getInfo()
+    console.log(this.data.uid)
+  },
+  onShow: function() {
 
-            wx.reLaunch({
-               url: '',
-               success: function(res) {
-                  wx.showToast({
-                     title: '支付成功',
-                     icon: 'none',
-                     duration: 2000,
-                  })
-               }
+  },
+  getInfo() {
+    let then = this
+    wx.request({
+      url: app.globalData.appUrl + 'drug/drug_num',
+      data: {
+        id: this.data.Id,
+        num: this.data.Num,
+        uid: wx.getStorageSync('user')
+      },
+      success: function(res) {
+        then.setData({
+          data: res.data
+        })
+      },
+      fail: function(res) {
+        wx.showToast({
+          title: '网络错误请稍后再试',
+          icon: 'none',
+          duration: 1000,
+        })
+      },
+    })
+  },
+  toAddress() {
+    wx.navigateTo({
+      url: '/pages/personal/address/address'
+    })
+  },
+  tjOrder() {
+
+    wx.request({
+      url: app.globalData.appUrl + 'drug/doctor_details',
+      data: {
+        uid: this.data.uid,
+        drug_id: this.data.Id,
+        num: this.data.Num
+      },
+      header: {},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (res) => {
+        console.log(res.data.id)
+        let id = res.data.id
+        wx.login({
+          success: (res) => {
+            console.log(res.code)
+            wx.request({
+              url: "https://jkxw.guaishe.com/index.php/index/pay/returnPay",
+              data: {
+                id: id,
+                code: res.code
+
+              },
+              header: {},
+              method: 'GET',
+              dataType: 'json',
+              responseType: 'text',
+              success: (res) => {
+                console.log(res)
+                let pay = res.data
+                let param = {
+                  "timeStamp": pay.timeStamp,
+                  "package": pay.package,
+                  "paySign": pay.paySign,
+                  "signType": pay.signType,
+                  "nonceStr": pay.nonceStr
+                };
+                this.payment(param)
+              },
+              fail: function(res) {
+                wx.showToast({
+                  title: '请求失败请稍后再试',
+                  icon: 'none',
+                  duration: 1000,
+                })
+              },
             })
-         },
-         fail: function(res) {
-            
-         },
-      })
-   }
+          },
+        })
+
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+    // wx.login({
+    //   success: (res) =>{
+    //     console.log(res.code)
+    //     wx.request({
+    //       url: "https://jkxw.guaishe.com/index.php/index/pay/returnPay",
+    //       data: {
+    //         id: this.data.Id,
+    //         code:res.code
+
+    //       },
+    //       header: {},
+    //       method: 'GET',
+    //       dataType: 'json',
+    //       responseType: 'text',
+    //       success:(res)=> {
+    //         console.log(res)
+    //         let pay = res.data
+    //         let param = {
+    //           "timeStamp": pay.timeStamp,
+    //           "package": pay.package,
+    //           "paySign": pay.paySign,
+    //           "signType": pay.signType,
+    //           "nonceStr": pay.nonceStr
+    //         };
+    //         this.payment(param)
+    //       },
+    //       fail: function(res) {
+    //         wx.showToast({
+    //           title: '请求失败请稍后再试',
+    //           icon: 'none',
+    //           duration: 1000,
+    //         })
+    //       },
+    //     })
+    //   },
+    // })
+
+  },
+  payment(param) {
+    wx.requestPayment({
+      timeStamp: param.timeStamp,
+      nonceStr: param.nonceStr,
+      package: param.package,
+      signType: param.signType,
+      paySign: param.paySign,
+      success: function(res) {
+
+        wx.reLaunch({
+          url: '',
+          success: function(res) {
+            wx.showToast({
+              title: '支付成功',
+              icon: 'none',
+              duration: 2000,
+            })
+          }
+        })
+      },
+      fail: function(res) {
+
+      },
+    })
+  }
 
 
 })
